295ada05d3e83917f6fd4477e38251da656a4d18
aizimpamvu <hallyiizza200@gmail.com>
2022-04-06 20:45:53 +0200

    car speed

--- main.py
+++ main.py
@@ -21,4 +21,15 @@ while game_is_on:
     car_manager.create_car()
     car_manager.move_cars()
 
-    #Detect a collusion
+    #Detect a car collusion
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on=False
+
+    #Detect a successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+
+screen.exitonclick()
